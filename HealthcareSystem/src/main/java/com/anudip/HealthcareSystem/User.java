6f863408f9c0442e6070aa2374d457191568d52c
amanbisen45 <amanbisen409@gmail.com>
2025-02-28 20:04:01 +0530

    First Commit

---
+++ HealthcareSystem/src/main/java/com/anudip/HealthcareSystem/User.java
@@ -0,0 +1,26 @@
+package com.anudip.HealthcareSystem;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
+
+enum Role {
+    PATIENT, DOCTOR, ADMIN
+}
